a6bbee6b9602a2aafc54e97c9a8e763c15bcc166
Ben Hays <benjaminjameshays@gmail.com>
2017-02-23 19:16:42 -0500

    test json = success

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+5.times do |num|
+   num+= 1
+   Post.create!(
+   title: "test post #{num}",
+   description: "Sample Description for post #{num}"
+   )
+end
